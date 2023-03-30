80e713499ab755ca4692a375fc42d199c0e5a481
Newcomer <59330076+hongthaiktv@users.noreply.github.com>
2023-03-30 12:32:56 +0000

    change domain name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 8080;
-const domain = 'https://onepage.herokuapp.com';
+const domain = 'https://onepage.cyclic.app';
 
 const admin = require('firebase-admin');
 const serviceAccount = require('./onepage-serviceAccountKey.json');
